ca5089b13a25e9f099e7600e3ec5b9b57c58e580
Abhigyan Trips <contact@abhigyantrips.dev>
2026-06-14 18:24:58 +0530

    chore(deploy): update worker name

---
+++ src/types.ts
@@ -0,0 +1,27 @@
+export type Page = {
+	TITLE: string;
+	DESCRIPTION: string;
+};
+
+export interface Site extends Page {
+	AUTHOR: string;
+	URL: string;
+}
+
+export type Route = {
+	NAME: string;
+	PATH: string;
+	ICON: string;
+	COLOR: {
+		LIGHT: string;
+		DARK: string;
+	};
+	DESCRIPTION: string;
+};
+
+export type Social = {
+	NAME: string;
+	TEXT: string;
+	COLOR: string;
+	HREF: string;
+};
