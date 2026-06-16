99cbb909781d24fa9eb07911a79f382b80a9d44f
Abhigyan Trips <contact@abhi.now>
2026-06-16 16:31:38 +0530

    chore(ui): replace display font with logo

--- astro.config.mjs
+++ astro.config.mjs
@@ -25,11 +25,6 @@ export default defineConfig({
   fonts: [
     {
       provider: fontProviders.fontsource(),
-      name: 'Jersey 10',
-      cssVariable: '--font-display',
-    },
-    {
-      provider: fontProviders.fontsource(),
       name: 'Maple Mono',
       cssVariable: '--font-mono',
     }
